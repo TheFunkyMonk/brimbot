46c8508e622e9e12ceebe717b46a4553057f242a
R.J. LaCount <rj@turnstyle.studio>
2024-08-12 09:35:24 -0700

    Clear timeout after fact successfully fetched

--- commands/cat-fact.js
+++ commands/cat-fact.js
@@ -4,15 +4,20 @@ export const name = "cat fact";
 export const description = "I tell you a random cat fact.";
 
 export default async function (message, args) {
+	let timeoutId;
+
 	try {
 		const response = await Promise.race([
-			fetch('https://catfact.ninja/fact'),
-			new Promise((_, reject) =>
-				setTimeout(() => {
-					reject(new Error('API is taking too long'))
+			new Promise((resolve, reject) => {
+				timeoutId = setTimeout(() => {
+					reject(new Error('API is taking too long'));
 					message.reply('Sorry, the API is taking too long and I\'m not very patient. Please try again later.');
-				}, 3000)
-			)
+				}, 3000);
+			}),
+			fetch('https://catfact.ninja/fact').then(res => {
+				clearTimeout(timeoutId); // Stop the timeout if fetch succeeds
+				return res;
+			})
 		]);
 
 		const data = await response.json();
